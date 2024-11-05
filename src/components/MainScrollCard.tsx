a1c247fdc6961cce96bf453af1a2788c82617828
JaehyunYoo <nike361@naver.com>
2024-11-05 19:01:25 +0900

    [fix]:2번째 section 추가

--- src/components/MainScrollCard.tsx
+++ src/components/MainScrollCard.tsx
@@ -19,28 +19,19 @@ export const MainScrollCard = ({
   targetScale,
   i,
 }: MainScrollCardProps) => {
-  // const container = useRef(null);
-
   const scale = useTransform(progress, range, [1, targetScale]);
 
   return (
-    <div className="h-[100vh] flex flex-col justify-center items-center sticky top-0">
+    <div className="w-full h-[100svh] flex flex-col justify-center items-center sticky top-0">
       <motion.div
-        className="bg-aqua flex flex-col relative h-[500px] w-[1000px] rounded-[25px] p-[50px] origin-top"
+        className="flex flex-col relative w-full h-full rounded-[25px] p-[50px] origin-top "
         style={{
-          backgroundColor: "#000",
           scale,
-          top: `calc(-5vh + ${i * 25}px)`,
+          top: `calc(1.5svh + ${i * 25}px)`,
         }}
       >
-        <h2>Title:test{i}</h2>
-        <div>
-          <p>Description:{i}</p>
-        </div>
-        <div>
-          <div>
-            <Image fill src={imageSrc} alt="image" />
-          </div>
+        <div className="">
+          <Image fill src={imageSrc} alt="image" />
         </div>
       </motion.div>
     </div>
